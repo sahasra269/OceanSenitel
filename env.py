1ae1ba0d9acc9e4f91c1a7ec28fefb32d57e0bae
sahasra269 <sahasra.9116@gmail.com>
2026-03-29 18:54:03 +0530

    Remove env.py from tracking

--- env.py
+++
@@ -1,9 +0,0 @@
-auth_cred = {
-    "API_KEY": "AIzaSyB320-_Z_wNhOLyuza5eztkB10dLovBsMU",
-    "AUTH_DOMAIN": "ocean-sentinel-19fb8.firebaseapp.com",
-    "DATABASE_URL": "https://ocean-sentinel-19fb8-default-rtdb.firebaseio.com",
-    "STORAGE_BUCKET": "ocean-sentinel-19fb8.firebasestorage.app",
-    "EMAIL": "sahasra.9116@gmail.com",  
-    "PASS": "Sahasra@26911",                 
-    "B_ID": "B-001"
-}
